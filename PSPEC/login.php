58ff24f148981e34ab1569d242d8fb469a4b1d14
mbandot <54519515+mbandot@users.noreply.github.com>
2019-09-14 00:16:25 +0700

    pspec Login.php

---
+++ PSPEC/login.php
@@ -0,0 +1,10 @@
+<?php ; 
+if (isset($_GET['er'])) { ?>
+    <div class="box-body">
+      <div class="alert alert-danger alert-dismissible">
+        <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+        <h4><i class="icon fa fa-ban"></i> Login Gagal!</h4>
+        Email atau Password salah..
+      </div>
+    </div>
+<?php } ?>
